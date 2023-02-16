99a04a65462aae1101b2ecb0b3bba67298b10334
Bondzeal <emmanuelgharteykb@gmail.com>
2023-02-16 06:26:20 -0800

    Writing a C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+/*
+  main is the entry text
+  program ends with 0 which means success
+ */ 
+
+int main () {
+    puts( "Programming is like building a multilingual puzzle\n" );
+    return 0;
+}
