65b12efda77f7802cfe26d5104ae9bcf3f3e5524
Bondzeal <emmanuelgharteykb@gmail.com>
2023-02-16 06:45:18 -0800

    Writing a C program agaiiinnnn

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/* main is the entry text
+/*
+ * main is the entry text
  * program ends with 0 which means success
  */ 
 int main(void) 
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
