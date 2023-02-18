7f7577eb2980997c9a860a587033531b71c282c5
Bondzeal <emmanuelgharteykb@gmail.com>
2023-02-17 17:16:56 -0800

    Excluding q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,13 @@
 int main(void)
 {
 	char lowercase;
-	
+
 	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 	{
 		if (lowercase != 'q' && lowercase != 'e')
 			putchar(lowercase);
 	}
 	putchar('\n');
+
 	return (0);
 }
