caea8f1b1f71013975a2c8a48c3c005c34ea99ef
Bondzeal <emmanuelgharteykb@gmail.com>
2023-02-23 22:03:54 -0800

    Largest printing

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -19,6 +19,7 @@ int largest_number(int a, int b, int c)
 	else if (a > c && c > b)
 	{
 		largest = a;
+	}
 	else if {b > a && a > c)
 	{
 		largest	= b;
@@ -26,6 +27,7 @@ int largest_number(int a, int b, int c)
 	else if (b > c && c > a)
 	{
 		largest = b;
+	}
 	else if (c > a && a > b)
 	{
 		largest = c;
