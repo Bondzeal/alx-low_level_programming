f405de4dede48be283c26351467a154334496675
Bondzeal <emmanuelgharteykb@gmail.com>
2023-03-02 10:35:04 -0800

    Task 5 fixed

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * sting_toupper - Changes all lowercase letters of a string to uppercase
+ * string_toupper - Changes all lowercase letters of a string to uppercase
  * @s: Manipulating string
  *
  * Return: Capitalization of all letters
