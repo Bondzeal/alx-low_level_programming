5416bae06684b17ac704740dae024f734c09f536
Bondzeal <emmanuelgharteykb@gmail.com>
2023-03-02 15:21:19 -0800

    13rot Encoding

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rot13 - Sting to encode in rot13
+ * @s: String that is to be encoded
+ *
+ * Return: Resulting string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char betalpha[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (j = 0; alphabet[j] != '\0'; j++)
+	{
+	if (s[i] == alphabt[j])
+	{
+	s[i] = betalpha[j];
+break;
+	}
+	}
+	}
+
+	return (s);
+}
