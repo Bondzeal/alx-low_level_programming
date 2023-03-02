a834d5ab7ae51d304cb2c736960a0d2ed9ddf9b7
Bondzeal <emmanuelgharteykb@gmail.com>
2023-03-02 07:45:04 -0800

    Changing letters in string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * sting_toupper - Changes all lowercase letters of a string to uppercase
+ * @s: Manipulating string
+ * Return: Capitalization of all letters
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+}
