ef6939a9908b82f2b601aebe2469fedf909bef21
Bondzeal <emmanuelgharteykb@gmail.com>
2023-03-06 13:54:54 -0800

    Locating a character in a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/** see  
+ * _str	pbrk - Searching for any set of bytes
+ * @s: first occurence in string, ie, string to search for
+ * @accept: bytes in the string to be searched for
+ *
+ * Return: a pointer to the byte in @s that matches
+ * one of thr bytes in @accept
+ * or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept);
+{
+	int a;
+
+	while (*s)
+	{
+		for (a = 0; accept[a]; a++)
+		{
+			if (*s == accept[a])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
