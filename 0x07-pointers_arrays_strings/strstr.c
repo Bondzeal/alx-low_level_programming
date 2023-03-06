f73dd44f449053b3c3edc62c49a17c30f741cef2
Bondzeal <emmanuelgharteykb@gmail.com>
2023-03-06 14:48:42 -0800

    Locating substring

---
+++ 0x07-pointers_arrays_strings/strstr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strstr - Locating a substring
+ * @needle: first occurrence of fhe substring
+ * @haystack: location of first occurrence
+ *
+ * Return: A pointer to the beginning of the located substring, or
+ * NULL if the substring is not found
+ *ear
+
