32a9f54283e7880ffe313740961406d6d8962f32
Bondzeal <emmanuelgharteykb@gmail.com>
2023-07-26 15:05:32 -0700

    Encoding a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - Function encodes a string using rot13
+ * @s: String that is to be encoded
+ *
+ * Return: Resulting string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	 char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	 char betalpha[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	 for (i = 0; s[i] != '\0'; i++)
+	 {
+		 for (j = 0; alphabet[j] != '\0'; j++)
+		 {
+			 if (s[i] == alphabet[j])
+			 {
+				 s[i] = betalpha[j];
+				 break;
+			 }
+		 }
+	 }
+
+	 return (s);
+}
