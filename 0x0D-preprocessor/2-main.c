e4a964e13c1c887da0ed147db649afbc562a0588
Bondzeal <emmanuelgharteykb@gmail.com>
2023-08-14 08:17:59 -0700

    Printing file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Function prints the name of a file it was compiled from
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("The file name is: %s\n", __FILE__);
+	return (0);
+}
